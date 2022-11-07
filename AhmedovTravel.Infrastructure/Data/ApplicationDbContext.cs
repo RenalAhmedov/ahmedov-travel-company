ba9051f68cb5f87ce5bfd5ec2a10420cfea5b95d
Renal Ahmedov <renalahmedov4@gmail.com>
2022-11-07 19:13:05 +0200

    updated dbcontext upload

--- AhmedovTravel.Infrastructure/Data/ApplicationDbContext.cs
+++ AhmedovTravel.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AhmedovTravel.Infrastructure.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AhmedovTravel.Infrastrucutre.Data
@@ -8,6 +9,38 @@ namespace AhmedovTravel.Infrastrucutre.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<UserDestination> UsersDestinations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserDestination>()
+                  .HasKey(ud => new { ud.UserId, ud.DestinationId });
+
+            builder.Entity<User>()
+                .Property(u => u.UserName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Entity<User>()
+               .Property(u => u.Email)
+               .HasMaxLength(60)
+               .IsRequired();
+
+
+
+
+
+
+
+
+
+            base.OnModelCreating(builder);
         }
     }
 }
