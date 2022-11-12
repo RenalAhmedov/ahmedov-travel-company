6744ae855fade176bef978345eb50067130d080c
Renal Ahmedov <renalahmedov4@gmail.com>
2022-11-12 18:40:20 +0200

    made the login and register views, configured the application cookie and password length. and added the new controller

---
+++ AhmedovTravel/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using AhmedovTravel.Infrastructure.DataConstants;
+using System.ComponentModel.DataAnnotations;
+
+namespace AhmedovTravel.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(UserConstants.UserNameMaxLength, MinimumLength = UserConstants.UserNameMinLength)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(UserConstants.EmailMaxLength, MinimumLength = UserConstants.EmailMinLength)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(UserConstants.PasswordMaxLength, MinimumLength = UserConstants.PasswordMinLength)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
--- AhmedovTravel/Program.cs
+++ AhmedovTravel/Program.cs
@@ -8,8 +8,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<User>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequiredLength = 5;
+})
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
