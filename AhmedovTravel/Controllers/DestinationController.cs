f81d379f527dacb99d17f50b2282d8a92771722c
Renal Ahmedov <renalahmedov4@gmail.com>
2022-11-13 19:39:03 +0200

    made a new controller and new views.

---
+++ AhmedovTravel/Controllers/DestinationController.cs
@@ -0,0 +1,59 @@
+﻿using AhmedovTravel.Core.Models.Destination;
+using AhmedovTravel.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AhmedovTravel.Controllers
+{
+    [Authorize]
+    public class DestinationController : Controller
+    {
+        [AllowAnonymous]
+        public async Task<IActionResult> All()
+        {
+            var model = new DestinationsQueryModel();
+
+            return View();
+        }
+
+        public async Task<IActionResult> Mine()
+        {
+            var model = new DestinationsQueryModel();
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Add(DestinationModel model)
+        {
+            int id = 1;
+
+            return RedirectToAction(nameof(All), new { id });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var model = new DestinationModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid id, DestinationModel model)
+        {
+            return RedirectToAction(nameof(All), new { id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return RedirectToAction(nameof(All));
+        }
+
+
+    }
+}
