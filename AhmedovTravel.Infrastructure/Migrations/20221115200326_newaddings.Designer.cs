22fb87fe90b5dcb1789a076e015abdf79a214de8
Renal Ahmedov <renalahmedov4@gmail.com>
2022-11-15 22:07:43 +0200

    adding migrations

--- AhmedovTravel.Infrastructure/Migrations/20221115200326_newaddings.Designer.cs
+++ AhmedovTravel.Infrastructure/Migrations/20221115200326_newaddings.Designer.cs
@@ -4,16 +4,18 @@ using AhmedovTravel.Infrastrucutre.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace AhmedovTravel.Infrastructure.migrations
+namespace AhmedovTravel.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221115200326_newaddings")]
+    partial class newaddings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
