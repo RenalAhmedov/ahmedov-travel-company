c218ed8846f613479daf8dbdaef60eb1475f5184
Renal Ahmedov <renalahmedov4@gmail.com>
2022-11-25 19:36:50 +0200

    renamed view and made the view work

--- AhmedovTravel.Core/Models/Transport/TransportViewModel.cs
+++ AhmedovTravel.Core/Models/Transport/TransportViewModel.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AhmedovTravel.Core.Models.Transport
+﻿namespace AhmedovTravel.Core.Models.Transport
 {
     public class TransportViewModel
     {
         public int Id { get; set; }
 
-        public string TransportType { get; set; }
+        public string TransportType { get; set; } = null;
 
-        public string ImageUrl { get; set; }
+        public string ImageUrl { get; set; } = null;
     }
 }
