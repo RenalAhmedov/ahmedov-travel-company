7bcf953efdad283b2646c61ea3c7f6f4636c25ce
Renal Ahmedov <renalahmedov4@gmail.com>
2022-12-01 15:25:11 +0200

    made the view models for the hotel.

---
+++ AhmedovTravel.Core/Models/Hotel/AddHotelViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AhmedovTravel.Infrastructure.DataConstants;
+using System.ComponentModel.DataAnnotations;
+
+namespace AhmedovTravel.Core.Models.Hotel
+{
+    public class AddHotelViewModel
+    {
+        [Required]
+        [StringLength(HotelConstants.HotelNameMaxLength), MinLength(HotelConstants.HotelNameMinLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(HotelConstants.HotelDescriptionMaxLength), MinLength(HotelConstants.HotelDescriptionMinLength)]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        //[Range(typeof(decimal), "0.0", "10.0", ConvertValueInInvariantCulture = true)]
+        public decimal HotelRating { get; set; }
+
+    }
+}
---
+++ AhmedovTravel.Core/Models/Hotel/HotelViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AhmedovTravel.Core.Models.Hotel
+{
+    public class HotelViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal HotelRating { get; set; }
+    }
+}
