6070e19fdf972a2db739a5976194049b77fdaef2
Renal Ahmedov <renalahmedov4@gmail.com>
2022-12-01 14:47:41 +0200

    started working on the hotel controller. and made some changes on the layout

---
+++ AhmedovTravel/Areas/Administrator/Controllers/HotelController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AhmedovTravel.Areas.Administrator.Controllers
+{
+    public class HotelController : Controller
+    {
+        [Area("Administrator")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AhmedovTravel/Controllers/HotelController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AhmedovTravel.Controllers
+{
+    [Authorize]
+    public class HotelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
