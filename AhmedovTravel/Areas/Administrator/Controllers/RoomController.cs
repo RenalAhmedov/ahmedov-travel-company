1da11a433527d7f9fea438d1b8fc5271f253f48d
Renal Ahmedov <renalahmedov4@gmail.com>
2022-12-02 14:23:48 +0200

    added roomcontroller in areas and room sections in layout.cshtml

---
+++ AhmedovTravel/Areas/Administrator/Controllers/RoomController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace AhmedovTravel.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    [Authorize(Roles = "Administrator")]
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
