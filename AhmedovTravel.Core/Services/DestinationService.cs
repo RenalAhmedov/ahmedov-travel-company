f886abe1755e1399ea149014b92ad0ec6947b8be
Renal Ahmedov <renalahmedov4@gmail.com>
2022-12-09 23:21:59 +0200

    addded coments in destination service

--- AhmedovTravel.Core/Services/DestinationService.cs
+++ AhmedovTravel.Core/Services/DestinationService.cs
@@ -15,6 +15,11 @@ namespace AhmedovTravel.Core.Services
             repo = _repo;
         }
 
+        /// <summary>
+        /// Creates a new Destination async
+        /// </summary>
+        /// <param name="model">View model containing the new Destination data</param>
+        /// <returns></returns>
         public async Task AddDestinationAsync(AddDestinationViewModel model)
         {
             var destination = new Destination()
@@ -29,6 +34,14 @@ namespace AhmedovTravel.Core.Services
             await repo.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Adds the destination to the user's collection 
+        /// </summary>
+        /// <param name="destinationId">Destination Id</param>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException">Throws if the given User doesn't exist</exception>
+        /// <exception cref="ArgumentException">Throws if the user's Destination collection has 1 or more destinations inside.</exception>
+        /// <exception cref="NullReferenceException">Throws if the given Destination doesn't exist</exception>
         public async Task AddDestinationToCollectionAsync(int destinationId, string userId)
         {
             var user = await repo.All<User>()
@@ -37,7 +50,7 @@ namespace AhmedovTravel.Core.Services
 
             if (user == null)
             {
-                throw new ArgumentException("Invalid user ID");
+                throw new NullReferenceException("Invalid user ID");
             }
 
             if (user.UsersDestinations.Count == 1)
@@ -50,7 +63,7 @@ namespace AhmedovTravel.Core.Services
 
             if (destination == null)
             {
-                throw new ArgumentException("Invalid Destination ID");
+                throw new NullReferenceException("Invalid Destination ID");
             }
 
             if (!user.UsersDestinations.Any(d => d.DestinationId == destinationId))
@@ -66,14 +79,31 @@ namespace AhmedovTravel.Core.Services
             await repo.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Sets a given Destination from Active to Inactive
+        /// </summary>
+        /// <param name="destinationId">Destination Id</param>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException">Throws if the given Destination doesn't exist.</exception>
         public async Task Delete(int destinationId)
         {
             var destination = await repo.GetByIdAsync<Destination>(destinationId);
+
+            if (destination == null)
+            {
+                throw new NullReferenceException();
+            }
+
             destination.IsActive = false;
 
             await repo.SaveChangesAsync();
         }
 
+
+        /// <summary>
+        /// Gets Id and Name for all active departments in the database
+        /// </summary>
+        /// <returns><AllDestinationsViewModel></returns>
         public async Task<AllDestinationsViewModel> DestinationDetailsById(int id)
         {
             return await repo.AllReadonly<Destination>()
@@ -92,9 +122,21 @@ namespace AhmedovTravel.Core.Services
                 .FirstAsync();
         }
 
+        /// <summary>
+        /// Edits existing Destination
+        /// </summary>
+        /// <param name="model">Model with the Edit data</param>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException">Throws if the given Department doesn't exist</exception>
         public async Task Edit(int destinationId, EditDestinationViewModel model)
         {
             var destination = await repo.GetByIdAsync<Destination>(destinationId);
+
+            if (destination == null)
+            {
+                throw new NullReferenceException();
+            }
+
 
             destination.Title = model.Title;
             destination.Town = model.Town;
@@ -105,12 +147,22 @@ namespace AhmedovTravel.Core.Services
             await repo.SaveChangesAsync();
         }
 
+
+
+        /// <summary>
+        /// Checks if the destination exists and the status is IsActive
+        /// </summary>
+        /// <returns><Bool></returns>
         public async Task<bool> Exists(int id)
         {
             return await repo.AllReadonly<Destination>()
               .AnyAsync(h => h.Id == id && h.IsActive);
         }
 
+        /// <summary>
+        /// Gets all active destinations in the database
+        /// </summary>
+        /// <returns>IEnumerable<AllDestinationsViewModel> destinations</returns>
         public async Task<IEnumerable<AllDestinationsViewModel>> GetAllAsync()
         {
             return await repo.AllReadonly<Destination>()
@@ -128,6 +180,13 @@ namespace AhmedovTravel.Core.Services
                .ToListAsync();
         }
 
+
+        /// <summary>
+        /// Removes a given destination from the user's collection
+        /// </summary>
+        /// <param name="destinationId">Destination Id</param>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException">Throws if the given Destination doesn't exist.</exception>
         public async Task RemoveDestinationFromCollectionAsync(int destinationId, string userId)
         {
             var user = await repo.All<User>()
@@ -136,7 +195,7 @@ namespace AhmedovTravel.Core.Services
 
             if (user == null)
             {
-                throw new ArgumentException("Invalid user ID");
+                throw new NullReferenceException("Invalid user ID");
             }
 
             var destination = user.UsersDestinations.FirstOrDefault(m => m.DestinationId == destinationId);
@@ -149,6 +208,12 @@ namespace AhmedovTravel.Core.Services
             }
         }
 
+
+        /// <summary>
+        /// Shows the user's destination collection
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException">Throws if the given User doesn't exist.</exception>
         public async Task<IEnumerable<MineDestinationsViewModel>> ShowDestinationCollectionAsync(string userId)
         {
             var user = await repo.All<User>()
@@ -158,7 +223,7 @@ namespace AhmedovTravel.Core.Services
 
             if (user == null)
             {
-                throw new ArgumentException("Invalid user ID");
+                throw new NullReferenceException("Invalid user ID");
             }
 
             return user.UsersDestinations
